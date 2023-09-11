2f95a1da915d2162a804c24baa188e1217a6ffc9
olamidemoney <ogunmakinolamide18@gmail.com>
2023-09-11 06:16:22 -0700

    g

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main put function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
+root@9020e1b7f21b:/alx-low_level_programming/0x00-hello_world# cat 5-printf.c 
+#include <stdio.h>
+/**
+ * main - this is a prinf function
+ * Return: 0
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
