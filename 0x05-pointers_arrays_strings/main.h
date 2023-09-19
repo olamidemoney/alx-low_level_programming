ba75855548f282349d7a9e489749c46055e12d5a
olamidemoney <ogunmakinolamide18@gmail.com>
2023-09-18 21:11:45 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
-#include <stdio.h>
 
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
